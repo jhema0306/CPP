1f9bd140025039c093664161de8059cb9749b367
jhema0306 <84710276+jhema0306@users.noreply.github.com>
2021-06-13 13:43:23 +0530

    Add files via upload

---
+++ prime3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,i,count=0;
+	cout<<"enter a number:";
+	cin>>n;
+	for(i=2;i<=sqrt(n+1);i++)
+	{
+		if(n%i==0)
+       	{
+       		cout<<"it is not a prime";
+       		count=1;
+       		break;
+       	}
+       }
+       if(count==0)
+          {
+          	cout<<"it is a prime";
+          	return 0;
+          }
+      }
